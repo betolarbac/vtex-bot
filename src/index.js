c1461a71a2a2b795dd4b1f20d5709731d86b1655
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2023-09-13 03:01:31 -0300

    correção de array

--- src/index.js
+++ src/index.js
@@ -55,9 +55,9 @@ client.on('interactionCreate', async (i) => {
 		{ name: 'Status Atual', value: `${data.status.description}` },
 		{ name: '\u200B', value: '\u200B' },
 		{ name: `${components[0].name}`, value: `${components[0].status === "operational" ? ':white_check_mark: ' : ':no_entry:'} ${components[0].status}`, inline: false },
-		{ name: `${components[1].name}`, value: `${components[0].status === "operational" ? ':white_check_mark: ' : ':no_entry:'} ${components[0].status}`, inline: false  },
-		{ name: `${components[2].name}`, value: `${components[0].status === "operational" ? ':white_check_mark: ' : ':no_entry:'} ${components[0].status}`, inline: false  },
-		{ name: `${components[3].name}`, value: `${components[0].status === "operational" ? ':white_check_mark: ' : ':no_entry:'} ${components[0].status}`, inline: false  },
+		{ name: `${components[1].name}`, value: `${components[1].status === "operational" ? ':white_check_mark: ' : ':no_entry:'} ${components[1].status}`, inline: false  },
+		{ name: `${components[2].name}`, value: `${components[2].status === "operational" ? ':white_check_mark: ' : ':no_entry:'} ${components[2].status}`, inline: false  },
+		{ name: `${components[3].name}`, value: `${components[3].status === "operational" ? ':white_check_mark: ' : ':no_entry:'} ${components[3].status}`, inline: false  },
 	)
 	.setImage('https://a.storyblok.com/f/194188/474x267/af3452f4b8/vtex-474x267.png')
 	.setTimestamp()
