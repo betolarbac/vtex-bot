28b06834bc0d5b05910cb5046ad73816f5995d90
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2023-09-13 00:25:10 -0300

    nova regra para pegar o id do server automaticamente

--- src/register-commands.js
+++ src/register-commands.js
@@ -1,24 +1,41 @@
 require("dotenv").config();
-const { REST,Routes  } = require('discord.js');
+const { Client, IntentsBitField, REST, Routes } = require('discord.js');
 
+const client = new Client({ 
+  intents: [
+      IntentsBitField.Flags.Guilds,
+      IntentsBitField.Flags.GuildMembers,
+      IntentsBitField.Flags.GuildMessages,
+      IntentsBitField.Flags.MessageContent,
+  ] 
+});
 
-const commands = [
-  {
-    name: 'status',
-    description: 'status da vtex',
-  },
-];
+client.on('ready', () => {
+  console.log(`Logado como ${client.user.tag}`);
+});
 
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+client.on('guildCreate', async (guild) => {
+  console.log(`Bot adicionado ao servidor: ${guild.name} (ID: ${guild.id})`);
 
-(async () => {
+  
+  const commands = [
+    {
+      name: 'status',
+      description: 'status da vtex',
+    },
+  ];
+
+  const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
   try {
     console.log('comando carregando');
-    await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), {
+    await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, guild.id), {
       body: commands,
     });
     console.log('comando registrando');
   } catch (error) {
     console.error(error);
   }
-})();
+});
+
+client.login(process.env.TOKEN);
